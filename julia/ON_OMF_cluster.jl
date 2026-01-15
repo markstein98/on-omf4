3fae7b7cfae3a614be30dc0bf88a5b7daad879da
markstein98 <marco.aliberti98@gmail.com>
2026-01-15 15:25:45 +0100

    Changed the parameter parsing from command line to toml file

--- julia/ON_OMF_cluster.jl
+++ julia/ON_OMF_cluster.jl
@@ -1,36 +1,56 @@
 using Dates
+using TOML
+
 include("ON_OMF_funcs_copies.jl")
 
+# Required fields
+required_keys = ["n_side_sites", "n_measurements", "n_HMC_steps", "dt", "n_indep_comps", "max_perturbative_order"]
+
+
 function print_usage()
-    println("Usage: $PROGRAM_FILE checkpoint_fname")
-    println("   or: $PROGRAM_FILE checkpoint_fname lat_file")
-    println("   or: $PROGRAM_FILE Npoint n_meas NHMC dt n_indep_comps max_ptord measure_every n_copies")
-    println("   or: $PROGRAM_FILE Npoint n_meas NHMC dt n_indep_comps max_ptord measure_every n_copies lat_file")
+    println("Usage: $PROGRAM_FILE config.toml")
+    println("Required keys in config file:")
+    println(required_keys)
+    println("See simulation_configurations/sample_configuration.toml for further details.")
 end
 
-function parse_args(args; floatType=Float32, intType=Int32)
-    possible_n_args = (1, 2, 8, 9)
-    n_args = length(args)
-    if !(n_args in possible_n_args)
-        print_usage()
-        exit(1)
+"""
+    parse_config(fname; floatType=Float32, intType=Int32)
+
+Reads and validates a TOML configuration file.
+"""
+function parse_config(fname; floatType=Float32, intType=Int32)
+    if !isfile(fname)
+        error("Configuration file not found: $fname")
     end
-    if n_args == 1 || n_args == 2
-        return args
+
+    cfg = TOML.parsefile(fname)
+
+    for key in required_keys
+        haskey(cfg, key) || error("Missing required config key: $key")
     end
-    Npoint = parse(intType, args[1])
-    n_meas = parse(intType, args[2])
-    NHMC = parse(intType, args[3])
-    dt = parse(floatType, args[4])
-    n_comps = parse(intType, args[5])
-    max_ptord = parse(intType, args[6])
-    measure_every = parse(intType, args[7])
-    n_copies = parse(intType, args[8])
-    if n_args < 9
-        return Npoint, n_meas, NHMC, dt, n_comps, max_ptord, measure_every, n_copies
-    end
-    return Npoint, n_meas, NHMC, dt, n_comps, max_ptord, measure_every, n_copies, args[9]
+
+    Npoint        = intType(cfg["n_side_sites"])
+    n_meas        = intType(cfg["n_measurements"])
+    NHMC          = intType(cfg["n_HMC_steps"])
+    dt            = floatType(cfg["dt"])
+    n_comps       = intType(cfg["n_indep_comps"])
+    max_ptord     = intType(cfg["max_perturbative_order"])
+
+    # Optional fields
+    measure_every = intType(get(cfg, "measure_every", 1))
+    n_copies      = intType(get(cfg, "n_copies", 1))
+    lat_file      = get(cfg, "energy_site_by_site_matlab", "")
+
+    return (Npoint, n_meas, NHMC, dt, n_comps, max_ptord, measure_every, n_copies, lat_file)
+end
+
+# ---- Program entry point ----
+
+if length(ARGS) != 1
+    print_usage()
+    exit(1)
 end
 
 println(now(), ": Library compilation successful.")
-@time launch_main_omf(parse_args(ARGS)...)
+@time launch_main_omf(parse_config(ARGS[1])...)
